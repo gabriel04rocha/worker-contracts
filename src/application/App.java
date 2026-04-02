ad78edea98ccc92dbd3a733a5ffa38337022101a
gabriel04rocha <gabriel04rocha@gmail.com>
2026-04-02 00:02:10 -0300

    feat: add user terminal interface

--- src/application/App.java
+++ src/application/App.java
@@ -1,46 +1,110 @@
 package application;
 
+import entities.*;
+import entities.enums.WorkerLevel;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-import entities.enums.WorkerLevel;
-import entities.*;
-
 public class App {
-    public static void main(String[] args) throws Exception {
 
-        Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
 
-        System.out.println("Para começar a usar o sistema, primeiro cadastre um trabalhador:");
+    List<Worker> workers = new ArrayList<>();
+    List<HourContract> contracts = new ArrayList<>();
+    List<Department> departments = new ArrayList<>();
 
-        System.out.print("Insira o nome do trabalhador: ");
+    String name;
+    WorkerLevel level;
+    Department department;
+    Double baseSalary;
+    Worker worker;
 
-        String name = sc.nextLine();
+    System.out.println(
+      "Para começar a usar o sistema, primeiro cadastre um trabalhador:"
+    );
 
-        System.out.println("Insira o o nível deste trabalhador dentre as opções descritas\n\n[JUNIOR]\n[MID_LEVEL]\n[SENIOR]\n");
+    System.out.print("Insira o nome do trabalhador: ");
 
-        System.out.print("Sua escolha: ");
+    name = sc.nextLine();
 
-        WorkerLevel level = WorkerLevel.valueOf(sc.next());
+    System.out.println(
+      "Insira o o nível deste trabalhador dentre as opções descritas\n\n[JUNIOR]\n[MID_LEVEL]\n[SENIOR]\n"
+    );
 
-        System.out.print("Insira o departamento deste trabalhador: ");
+    System.out.print("Sua escolha: ");
 
-        sc.nextLine();
+    level = WorkerLevel.valueOf(sc.next());
 
-        Department department = new Department(sc.nextLine());
+    System.out.print("Insira o departamento deste trabalhador: ");
 
-        System.out.print("Insira agora o salário base deste trabalhador: ");
+    sc.nextLine();
 
-        Double baseSalary = sc.nextDouble();
+    department = new Department(sc.nextLine());
 
-        int option = 0;
+    System.out.print("Insira agora o salário base deste trabalhador: ");
 
-        do {
+    baseSalary = sc.nextDouble();
 
-            System.out.println("\nO que você deseja fazer agora?\n[1] Mudar o departamento do trabalhador\n[2] Alterar o salário do trabalhador\n[3] Criar um novo contrato");
+    worker = new Worker(name, level, baseSalary, department);
 
-        } while (option != 9);
-        
-        sc.close();
+    workers.add(worker);
 
-    }
+    int option = 0;
+
+    do {
+      System.out.println("O que você deseja fazer agora?\n");
+      System.out.println(
+        "---------------------TRABALHADORES---------------------"
+      );
+      System.out.println(
+        "\n[1] Cadastrar um novo trabalhador\n[2] Ver todos os trabalhadores cadastrados\n[3] Alterar um trabalhador\n[4] Mudar o departamento de um trabalhador\n[5] Excluir trabalhadores existentes\n"
+      );
+      System.out.println("---------------------CONTRATOS---------------------");
+      System.out.println(
+        "\n[6] Cadastrar um novo contrato\n[7] Ver todos os contratos cadastrados\n[8] Editar contratos\n[9] Deletar contratos existentes\n"
+      );
+      System.out.println(
+        "---------------------DEPARTAMENTOS---------------------"
+      );
+      System.out.println(
+        "\n[10] Cadastrar um novo departamento\n[11] Ver todos os departamentos cadastrados\n[12] Editar departamentos\n[13] Deletar departamentos existentes\n"
+      );
+      System.out.print("Sua escolha: ");
+
+      option = sc.nextInt();
+
+      switch (option) {
+        case 1:
+          System.out.print("Insira o nome do trabalhador: ");
+
+          name = sc.nextLine();
+
+          System.out.println(
+            "Insira o o nível deste trabalhador dentre as opções descritas\n\n[JUNIOR]\n[MID_LEVEL]\n[SENIOR]\n"
+          );
+
+          System.out.print("Sua escolha: ");
+
+          level = WorkerLevel.valueOf(sc.next());
+
+          System.out.print("Insira o departamento deste trabalhador: ");
+
+          sc.nextLine();
+
+          department = new Department(sc.nextLine());
+
+          System.out.print("Insira agora o salário base deste trabalhador: ");
+
+          baseSalary = sc.nextDouble();
+
+          break;
+        default:
+          break;
+      }
+    } while (option != 9);
+
+    sc.close();
+  }
 }
